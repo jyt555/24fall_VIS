36a7621b0356f5c6bddb7e3db99da82155a2a044
jyt555 <1369218489@qq.com>
2024-12-19 18:20:53 +0800

    散点图编码颜色信息

--- src/components/ScatterChart.vue
+++ src/components/ScatterChart.vue
@@ -27,31 +27,38 @@ export default {
         return { datasets: [] };
       }
 
+      // 根据电影名称设置颜色
+      const backgroundColors = this.movies.map(movie => {
+        const color = this.getColorByMovieName(movie.Series_Title);
+        console.log(`Color for ${movie.Series_Title}: ${color}`); // 调试输出
+        return color;
+      });
+
+      // const backgroundColors = "#ffccff";
+
       const data = {
-        datasets: []
+        datasets: [
+        {
+          label: 'Movies',
+          backgroundColor: backgroundColors,
+          data: this.movies.map((movie) => {
+            const imdbRating = parseFloat(movie.IMDB_Rating);
+            const yValue = this.getYValue(movie);
+            // 过滤掉非法数据
+            if (isNaN(imdbRating) || isNaN(yValue)) {
+              return null; // 返回 null 以排除掉无效数据
+            }
+            return {
+              x: imdbRating,  // 使用 IMDB_Rating 作为 X 轴数据
+              y: yValue,      // 根据 yLabel 来获取 Y 轴数据
+            };
+          }).filter(item => item !== null), // 过滤掉 null 值
+          pointRadius: 5
+        }]
       };
 
-      const dataset = {
-        label: 'Movies',
-        data: this.movies.map(movie => {
-          const imdbRating = parseFloat(movie.IMDB_Rating);
-          const yValue = this.getYValue(movie);
-          const color = "#66ccff"
-          // 过滤掉非法数据
-          if (isNaN(imdbRating) || isNaN(yValue)) {
-            return null; // 返回 null 以排除掉无效数据
-          }
-          return {
-            x: imdbRating,  // 使用 IMDB_Rating 作为 X 轴数据
-            y: yValue,      // 根据 yLabel 来获取 Y 轴数据
-            pointBackgroundColor: color
-          };
-        }).filter(item => item !== null), // 过滤掉 null 值
-        pointRadius: 5
-      };
-
-      data.datasets.push(dataset);
       return data;
+    
     }
   },
   methods: {
@@ -69,9 +76,21 @@ export default {
           return 0;
       }
     },
+    getColorByMovieName(title) {
+      // 根据电影名称在this.movies中找到对应的颜色
+      const movie = this.movies.find(item => item.Series_Title === title);
+      if (!movie) {
+        return '#66ccff';
+      }
+      const color = movie.color;
+      console.log('Find:', movie, color); // 调试输出
+      return color;
+    },
+    
   }
 };
 </script>
+
 
 <style scoped>
 .scatter-chart {
